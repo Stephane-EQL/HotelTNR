786798def4e197a816840a29ec16869fd28573c4
Stephane-EQL <stephane.raes@gmail.com>
2020-02-18 16:00:26 +0100

    change driver

--- src/test/java/jenkins/HotelWebapp/AppTest.java
+++ src/test/java/jenkins/HotelWebapp/AppTest.java
@@ -27,18 +27,11 @@ public class AppTest {
 	Main main = PageFactory.initElements(driver, Main.class);
 
 	@Before
-	public void SetUp() throws InterruptedException {
+	public void Driver() throws InterruptedException {
 
-		if(browser.equals("Chrome"))	{
-			driver = new ChromeDriver();
-			System.setProperty("webdriver.chromedriver", "/HotelWebapp/src/drivers/chromedriver.exe");
-		}
-		if(browser.equals("Firefox"))	{
-			driver = new FirefoxDriver();
-			System.setProperty("webdriver.geckodriver", "/HotelWebapp/src/drivers/geckodriver.exe");
-		}			
-	}	
-
+		WebDriver driver = new ChromeDriver();
+	}
+	
 	@Test	
 	public void Connexion () throws InterruptedException, IOException {
 
@@ -50,9 +43,7 @@ public class AppTest {
 
 		Assert.assertEquals("Application Tutoriel pour la base de données des hotels" , main.titleh2.getText());		
 		Assert.assertEquals("Choisissez une destination" , main.titleh3.getText());
-	
-	
-	
+		
 		Select cities = new Select (main.select_city);
 		cities.selectByVisibleText("Tokyo");
 		main.submit_button.click();
