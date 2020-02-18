f80d0a6a0a6d033abe3df265a64e1c149c8d8f47
Stephane-EQL <stephane.raes@gmail.com>
2020-02-18 15:56:18 +0100

    dependences bis

--- src/test/java/jenkins/HotelWebapp/AppTest.java
+++ src/test/java/jenkins/HotelWebapp/AppTest.java
@@ -1,12 +1,13 @@
 package jenkins.HotelWebapp;
 
 import java.io.IOException;
+
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
